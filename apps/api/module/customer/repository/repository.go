59a02e28c66e39f20b4d4506b5de0d77c0bffc68
S1nceU <jjkk900925@gmail.com>
2025-03-13 22:04:53 +0800

    Modify all of model's structural detail and optimization DB operate

--- apps/api/module/customer/repository/repository.go
+++ apps/api/module/customer/repository/repository.go
@@ -18,48 +18,57 @@ func NewCustomerRepository(orm *gorm.DB) domain.CustomerRepository {
 
 func (u *CustomerRepository) ListCustomers() ([]*model.Customer, error) {
 	var customers []*model.Customer
-	err := u.orm.Find(&customers).Error
+	err := u.orm.Preload("Citizenship").Find(&customers).Error
 	return customers, err
 }
 
 func (u *CustomerRepository) ListCustomersByCitizenship(customer *model.Customer) ([]*model.Customer, error) {
 	var customers []*model.Customer
-	err := u.orm.Where("CitizenshipId = ?", customer.CitizenshipId).Find(&customers).Error
+	err := u.orm.Preload("Citizenship").Where("CitizenshipId = ?", customer.CitizenshipId).Find(&customers).Error
 	return customers, err
 }
 
-func (u *CustomerRepository) ListCustomersByCustomerName(customer *model.Customer) ([]*model.Customer, error) {
+func (u *CustomerRepository) ListCustomersByName(customer *model.Customer) ([]*model.Customer, error) {
 	var customers []*model.Customer
 	err := u.orm.Where("Name LIKE ?", "%"+customer.Name+"%").Find(&customers).Error
 	return customers, err
 }
 
-func (u *CustomerRepository) ListCustomersByCustomerPhone(customer *model.Customer) ([]*model.Customer, error) {
+func (u *CustomerRepository) ListCustomersByPhone(customer *model.Customer) ([]*model.Customer, error) {
 	var customers []*model.Customer
 	err := u.orm.Where("PhoneNumber LIKE ?", "%"+customer.PhoneNumber+"%").Find(&customers).Error
 	return customers, err
 }
 
 func (u *CustomerRepository) GetCustomerByNationalId(customer *model.Customer) (*model.Customer, error) {
-	err := u.orm.Where("NationalId = ?", customer.NationalId).Find(&customer).Error
+	err := u.orm.Preload("Citizenship").Preload("Histories").Where("NationalId = ?", customer.NationalId).Find(&customer).Error
 	return customer, err
 }
 
 func (u *CustomerRepository) GetCustomerByCustomerId(customer *model.Customer) (*model.Customer, error) {
-	err := u.orm.Where("Id = ?", customer.Id).Find(&customer).Error
+	err := u.orm.Preload("Citizenship").Preload("Histories").Where("Id = ?", customer.Id).Find(&customer).Error
 	return customer, err
 }
 
 func (u *CustomerRepository) CreateCustomer(customer *model.Customer) (*model.Customer, error) {
 	err := u.orm.Create(customer).Error
+	if err != nil {
+		return nil, err
+	}
+	err = u.orm.Preload("Citizenship").First(&customer, "id = ?", customer.Id).Error
 	return customer, err
 }
 
 func (u *CustomerRepository) UpdateCustomer(customer *model.Customer) (*model.Customer, error) {
 	err := u.orm.Model(customer).Where("Id = ?", customer.Id).Updates(&customer).Error
+	if err != nil {
+		return nil, err
+	}
+	err = u.orm.Preload("Citizenship").First(&customer, "id = ?", customer.Id).Error
 	return customer, err
 }
 
 func (u *CustomerRepository) DeleteCustomer(customer *model.Customer) error {
+	u.orm.Where("CustomerId = ?", customer.Id).Delete(&model.History{})
 	return u.orm.Where("Id = ?", customer.Id).Delete(&customer).Error
 }
